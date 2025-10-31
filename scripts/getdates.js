0f1c67d8b96aec40ead549872418ac4377b7edce
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-10-30 19:54:08 -0500

    fixed it.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,9 +4,8 @@ currentyeardoc.innerHTML = yeartoday;
 
 const lastModified = new Date(document.lastModified)
 const modyear = lastModified.getFullYear();
-const modmonth = lastModified.getMonth();
-const modday = lastModified.getDay();
+const modmonth = lastModified.getMonth()+1;
+const modday = lastModified.getDate();
 const moddate = `${modmonth}/${modday}/${modyear}`
-console.log(moddate);
 const lastModifiedElement = document.getElementById("lastmodified")
 lastModifiedElement.textContent = moddate;
