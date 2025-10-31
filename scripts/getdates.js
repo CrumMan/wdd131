4498783f7ee135177fff2470ff86c2869ef42a2a
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-10-30 19:52:06 -0500

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,6 +2,11 @@ const yeartoday = new Date().getFullYear();
 const currentyeardoc = document.getElementById("currentyear");
 currentyeardoc.innerHTML = yeartoday;
 
-const lastModified = document.lastModified
+const lastModified = new Date(document.lastModified)
+const modyear = lastModified.getFullYear();
+const modmonth = lastModified.getMonth();
+const modday = lastModified.getDay();
+const moddate = `${modmonth}/${modday}/${modyear}`
+console.log(moddate);
 const lastModifiedElement = document.getElementById("lastmodified")
-lastModified.textContent = lastModified
+lastModifiedElement.textContent = moddate;
