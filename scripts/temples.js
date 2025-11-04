6983e6fe8243dd2f533a31044d4fd2c3acbe3f5c
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-11-04 15:39:10 -0600

    fixed js errors

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,12 +1,13 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
+const navLink = document.querySelectorAll('.nav-link')
 
 hamburger.addEventListener("click", ()=> {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
-document.querySelector('.nav-link').forEach(n => n.addEventListener("click", ()=> {
+navLink.forEach(n => n.addEventListener("click", ()=> {
     hamburger.classList.remove('active')
     navMenu.classList.remove('active')
 }))
