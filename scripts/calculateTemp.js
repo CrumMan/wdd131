e38f3e3afa661b282184053db30e2170c2bd9604
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-11-13 12:48:23 -0600

    fixed calculate temp to have svg image

--- scripts/calculateTemp.js
+++ scripts/calculateTemp.js
@@ -3,24 +3,25 @@ const typeElem = document.getElementById('type');
 const type = typeElem.textContent.toLowerCase();
 
 const weatherImg = document.getElementById('weather_photo');
-
-switch (type) {
-    case 'sunny':
-        weatherImg.src = './images/weather/sunny.webp';
-        break;
-    case 'rainy':
-        weatherImg.src = './images/weather/rainy.webp';
-        break;
-    case 'cloudy':
-        weatherImg.src = './images/weather/cloudy.webp';
-        break;
-    case 'snowy':
-        weatherImg.src = './images/weather/snowy.webp' ;
-        break;
-    case 'tornado warning':
-        weatherImg.src = './images/weather/tornado.webp';
-        break;
-    default:
-        weatherImg.src = './images/weather/default.webp';
-        break;
+if(!weatherImg.src){
+    switch (type) {
+        case 'sunny':
+            weatherImg.src = './images/weather/sunny.webp';
+            break;
+        case 'rainy':
+            weatherImg.src = './images/weather/rainy.webp';
+            break;
+        case 'cloudy':
+            weatherImg.src = './images/weather/cloudy.webp';
+            break;
+        case 'snowy':
+            weatherImg.src = './images/weather/snowy.webp' ;
+            break;
+        case 'tornado warning':
+            weatherImg.src = './images/weather/tornado.webp';
+            break;
+        default:
+            weatherImg.src = './images/weather/default.webp';
+            break;
+    }
 }
