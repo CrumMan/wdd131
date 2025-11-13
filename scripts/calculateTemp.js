1c5a68e17c6014cf5f4cd8ccf43fa9abf3a96a7f
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-11-13 12:17:35 -0600

    changes for speed

--- scripts/calculateTemp.js
+++ scripts/calculateTemp.js
@@ -6,21 +6,21 @@ const weatherImg = document.getElementById('weather_photo');
 
 switch (type) {
     case 'sunny':
-        weatherImg.src = './images/weather/sunny.jpg';
+        weatherImg.src = './images/weather/sunny.webp';
         break;
     case 'rainy':
-        weatherImg.src = './images/weather/rainy.jpg';
+        weatherImg.src = './images/weather/rainy.webp';
         break;
     case 'cloudy':
-        weatherImg.src = './images/weather/cloudy.jpg';
+        weatherImg.src = './images/weather/cloudy.webp';
         break;
     case 'snowy':
-        weatherImg.src = './images/weather/snowy.jpg' ;
+        weatherImg.src = './images/weather/snowy.webp' ;
         break;
     case 'tornado warning':
-        weatherImg.src = './images/weather/tornado.jpg';
+        weatherImg.src = './images/weather/tornado.webp';
         break;
     default:
-        weatherImg.src = './images/weather/default.jpg';
+        weatherImg.src = './images/weather/default.webp';
         break;
 }
