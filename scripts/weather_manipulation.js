63081885cb63ae73e96bb4acd3af43a9530aa34d
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-11-13 13:01:48 -0600

    created alteror for windchill

--- scripts/weather_manipulation.js
+++ scripts/weather_manipulation.js
@@ -25,3 +25,8 @@ if(!weatherImg.src){
             break;
     }
 }
+
+const windspeed = document.getElementById('Windspeed')
+const temprature = document.getElementById('avgTemp')
+
+document.getElementById('Windchill').innerText = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16);
