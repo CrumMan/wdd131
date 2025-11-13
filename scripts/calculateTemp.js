148a21d078b3598c738fcc9ff44fe8fe485cb9d9
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-11-13 11:55:45 -0600

    small fix?

--- scripts/calculateTemp.js
+++ scripts/calculateTemp.js
@@ -6,20 +6,20 @@ const weatherImg = document.getElementById('weather_photo');
 
 switch (type) {
     case 'sunny':
-        weatherImg.src = '/images/weather/sunny.jpg';
+        weatherImg.src = './images/weather/sunny.jpg';
         break;
     case 'rainy':
-        weatherImg.src = '/images/weather/rainy.jpg';
+        weatherImg.src = './images/weather/rainy.jpg';
         break;
     case 'cloudy':
-        weatherImg.src = '/images/weather/cloudy.jpg';
+        weatherImg.src = './images/weather/cloudy.jpg';
         break;
     case 'snowy':
-        weatherImg.src = '/images/weather/snowy.jpg' 
+        weatherImg.src = './images/weather/snowy.jpg' 
     case 'tornado warning':
-        weatherImg.src = '/images/weather/tornado.jpg'
+        weatherImg.src = './images/weather/tornado.jpg'
         break;
     default:
-        weatherImg.src = '/images/weather/default.jpg';
+        weatherImg.src = './images/weather/default.jpg';
         break;
 }
