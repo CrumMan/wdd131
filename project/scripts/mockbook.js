6649a7ae2dccaa0f2d74ba323fd3c0266a789d29
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-12-02 13:32:53 -0600

    final update for today

--- project/scripts/mockbook.js
+++ project/scripts/mockbook.js
@@ -97,25 +97,3 @@ function makeFriendList(friendArray, content){
     })
 }
 createRightAction()
-function LoginOrLogout(){
-    const user = localStorage.getItem("user")
-    const text = document.getElementById("loginLogoutToggle")
-
-    if(user){
-        text.innerHTML= `<a id="LogoutToggle" href=./profile.html>Logout</a>`
-
-    }
-    else {
-        text.innerHTML = `<a id="LoginToggle" href=./profile.html>Login</a>`
-    }
-
-    const logout = document.querySelector("#LogoutToggle")
-    if (logout){
-
-        logout.addEventListener("click", () => {
-        localStorage.removeItem("user")
-        location.reload()
-        })
-    }
-}
-LoginOrLogout()
--- project/scripts/profile.js
+++ project/scripts/profile.js
@@ -20,27 +20,3 @@ else {
     }
 }
 createUser()
-
-function LoginOrLogout(){
-    const user = localStorage.getItem("user")
-    const text = document.getElementById("loginLogoutToggle")
-
-    if(user){
-        text.innerHTML= `<a id="LogoutToggle" href=./profile.html>Logout</a>`
-        document.querySelector(".")
-
-    }
-    else {
-        text.innerHTML = `<a id="LoginToggle" href=./profile.html>Login</a>`
-    }
-
-    const logout = document.querySelector("#LogoutToggle")
-    if (logout){
-
-        logout.addEventListener("click", () => {
-        localStorage.removeItem("user")
-        location.reload()
-        })
-    }
-}
-LoginOrLogout()
