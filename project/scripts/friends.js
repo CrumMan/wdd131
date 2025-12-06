84cfa01a2faae43a59ec3f0c7458239b2bb8a34d
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-12-05 20:36:39 -0600

    remade js with map to make faster

--- project/scripts/friends.js
+++ project/scripts/friends.js
@@ -3,7 +3,7 @@ const friends = ["John Murphy", "Larry the Cable Guy", "Old Saint Nick", "Richar
 const suggestedFriends = ["Fabio Lounges", "Jason Vorhees", "Vicky Beans", "Pablo Escobar", "Hakeem Jeffries", "Lana Del Ray"]
 
 const friendDoc = document.querySelector(".friends")
-const suggestedFriendsDoc = document.querySelector(".suggested_friends")
+const suggestedFriendsDoc = document.querySelector(".suggested_friends ul")
 
 makeFriendList(friends,friendDoc)
 makeFriendList(suggestedFriends, suggestedFriendsDoc)
@@ -12,9 +12,10 @@ friendDoc
 
 }
 function makeFriendList(friendArray, content){
-    friendArray.map(friend => {
+    const html = friendArray.map(friend => {
         const filename = friend.trim().toLowerCase().replace(/ /g, "")
-        return `<li class="friendEl">${friend}<br><img loading="lazy" src="./images/${filename}.webp"></li>`;
-    })
+        return `<li class="friendEl">${friend}<br><img class="friendElImg loading="lazy" src="./images/${filename}.webp"></li>`;
+    }).join('')
+    content.innerHTML = html
 }
 createRightAction()
